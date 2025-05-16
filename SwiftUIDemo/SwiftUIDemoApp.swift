256e6ac997ea41e800aecfd69e77fc6ae43f8856
Eslam Gamal <eslam.gamal@softxpert.com>
2025-05-16 15:12:03 +0300

    Initial Commit

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Eslam Gamal on 16/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
